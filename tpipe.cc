bf994a66feca8c206836771ceb782b5e9075d3d2
Marc Fehling <mafehling.git@gmail.com>
2021-04-20 18:25:51 -0600

    Use GridGenerator functions instead of hard-coded mesh description for reference cylinder.

--- tpipe.cc
+++ tpipe.cc
@@ -173,114 +173,39 @@ create_reference_cylinder(const bool                do_transition,
                           std::vector<Point<3>> &   vertices_3d,
                           std::vector<CellData<3>> &cell_data_3d)
 {
+  // ???
   if (do_transition)
     printf("WARNING: Transition has not been implemented yet (TODO)!\n");
 
-  // position of auxiliary point to achieve an angle of 120 degrees in corner
-  // of inner cell
-  const double radius = 1;
-  const double y_coord =
-    0.55 * std::sqrt(0.5) * radius * std::cos(numbers::PI / 12) /
-    (std::sin(numbers::PI / 12) + std::cos(numbers::PI / 12));
-  // vertices for quarter of circle
-  std::vector<Point<2>> vertices{{0, 0},
-                                 {0.5 * radius, 0},
-                                 {y_coord, y_coord},
-                                 {radius, 0},
-                                 {radius * 0.5, radius * 0.5}};
+  // create base circle
+  const double     radius = 1.;
+  const Point<2>   center;
+  Triangulation<2> circle;
+  GridGenerator::hyper_ball_balanced(circle, center, radius);
 
+  // extrude to cylinder
+  const unsigned int n_slices = n_sections + 1;
+  const double       height   = 1.;
+  Triangulation<3>   cylinder;
+  GridGenerator::extrude_triangulation(
+    circle, n_slices, height, cylinder, /*copy_manifold_ids=*/false);
 
-  // create additional vertices for other three quarters of circle -> gives 17
-  // vertices in total
-  for (unsigned int a = 1; a < 4; ++a)
-    {
-      Tensor<2, 2> transform;
-      transform[0][0] = a == 2 ? -1. : 0;
-      transform[1][0] = a == 2 ? 0 : (a == 1 ? 1 : -1);
-      transform[0][1] = -transform[1][0];
-      transform[1][1] = transform[0][0];
-      for (unsigned int i = 1; i < 5; ++i)
-        vertices.emplace_back(transform * vertices[i]);
-    }
-
-  // create 12 cells for 2d mesh on base; the first four elements are at the
-  // center of the circle
-  std::vector<CellData<2>> cell_data(12);
-  cell_data[0].vertices[0] = 0;
-  cell_data[0].vertices[1] = 1;
-  cell_data[0].vertices[2] = 5;
-  cell_data[0].vertices[3] = 2;
-  cell_data[1].vertices[0] = 9;
-  cell_data[1].vertices[1] = 0;
-  cell_data[1].vertices[2] = 6;
-  cell_data[1].vertices[3] = 5;
-  cell_data[2].vertices[0] = 10;
-  cell_data[2].vertices[1] = 13;
-  cell_data[2].vertices[2] = 9;
-  cell_data[2].vertices[3] = 0;
-  cell_data[3].vertices[0] = 13;
-  cell_data[3].vertices[1] = 14;
-  cell_data[3].vertices[2] = 0;
-  cell_data[3].vertices[3] = 1;
-
-  // the next 8 elements describe the rim; we take one quarter of the circle
-  // in each loop iteration
-  for (unsigned int a = 0; a < 4; ++a)
-    {
-      cell_data[4 + a * 2].vertices[0] = 1 + a * 4;
-      cell_data[4 + a * 2].vertices[1] = 3 + a * 4;
-      cell_data[4 + a * 2].vertices[2] = 2 + a * 4;
-      cell_data[4 + a * 2].vertices[3] = 4 + a * 4;
-      cell_data[5 + a * 2].vertices[0] = 2 + a * 4;
-      cell_data[5 + a * 2].vertices[1] = 4 + a * 4;
-      AssertIndexRange(4 + a * 4, vertices.size());
-      cell_data[5 + a * 2].vertices[2] = a == 3 ? 1 : 5 + a * 4;
-      cell_data[5 + a * 2].vertices[3] = a == 3 ? 3 : 7 + a * 4;
-    }
-  SubCellData subcell_data;
-  GridTools::consistently_order_cells(cell_data);
-
-  Triangulation<2> tria_2d;
-  tria_2d.create_triangulation(vertices, cell_data, subcell_data);
-
-  vertices_3d.clear();
-  vertices_3d.resize((n_sections + 1) * tria_2d.n_vertices());
-  cell_data_3d.clear();
-  cell_data_3d.resize(n_sections * cell_data.size());
-
-  for (unsigned int s = 0; s <= n_sections; s++)
-    {
-      const double       beta  = (1.0 * s) / n_sections;
-      const unsigned int shift = s * tria_2d.n_vertices();
-      for (unsigned int i = 0; i < tria_2d.n_vertices(); ++i)
-        {
-          vertices_3d[shift + i][0] = tria_2d.get_vertices()[i][0];
-          vertices_3d[shift + i][1] = tria_2d.get_vertices()[i][1];
-          vertices_3d[shift + i][2] = beta;
-        }
-    }
-
-
-  for (unsigned int s = 0; s < n_sections; s++)
-    for (unsigned int i = 0; i < cell_data.size(); ++i)
-      {
-        for (unsigned int v = 0; v < 4; ++v)
-          cell_data_3d[cell_data.size() * s + i].vertices[v + 0] =
-            (s + 0) * vertices.size() + cell_data[i].vertices[v];
-        for (unsigned int v = 0; v < 4; ++v)
-          cell_data_3d[cell_data.size() * s + i].vertices[4 + v] =
-            (s + 1) * vertices.size() + cell_data[i].vertices[v];
-      }
+  // extract coarse mesh description (as return parameters)
+  const auto coarse_mesh = GridTools::get_coarse_mesh_description(cylinder);
+  vertices_3d            = std::get<0>(coarse_mesh);
+  cell_data_3d           = std::get<1>(coarse_mesh);
 
 #ifdef DEBUG
   // create triangulation
-  Triangulation<3> tria(Triangulation<3>::MeshSmoothing::none, true);
+  Triangulation<3> tria(Triangulation<3>::MeshSmoothing::none,
+                        /*check_for_distorted_cells=*/true);
 
+  SubCellData subcell_data = std::get<2>(coarse_mesh);
   tria.create_triangulation(vertices_3d, cell_data_3d, subcell_data);
-  GridOut gridout;
 
   std::ofstream out("mesh_reference_cylinder.vtu");
 
+  GridOut gridout;
   gridout.write_vtu(tria, out);
 #endif
 }
